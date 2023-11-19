6f7c2d7100322210cc14ca7a28438c314167a0a4
Alexkin <sacro_ml@hotmail.com>
2023-11-19 02:22:34 -0600

    Feature: Stetic updates in printed console results.

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ app = express();
 app.route('/jwt').get((req, res) => {
   let jwtValue = generateJWT();
 
-  console.log('--------------------------------------------------------------------------');
-  console.log('Key generated on ' + new Date());
-  console.log('--------------------------------------------------------------------------');
+  console.log('');
+  console.log('+----------------------------------------------------------------------------+');
+  console.log('| Key generated on ' + new Date() + ' |');
+  console.log('+----------------------------------------------------------------------------+');
   console.log(jwtValue);
-
+  
   res.json({ jwtValue });
 });
 
